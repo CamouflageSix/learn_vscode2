b06846fa9a682be91c62a426fbf427fda005885d
CamouflageSix <y-mitani@sweets-factory.biz>
2019-08-16 12:19:15 +0900

    master to develop

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main(int, char**)
+{
+    std::cout << "Hello, world!" << std::endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "Sample For Loop : " << i << std::endl;
+    }
+
 }
