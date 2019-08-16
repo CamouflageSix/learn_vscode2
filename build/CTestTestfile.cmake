b475bf801761c661e0594a5d60488a50b42f8749
CamouflageSix <y-mitani@sweets-factory.biz>
2019-08-16 10:59:32 +0900

    new

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/camouflagesix/git/learn_vscode2
+# Build directory: /home/camouflagesix/git/learn_vscode2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
